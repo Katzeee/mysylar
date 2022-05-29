425074535e99130585696a9b1cfce39f54d60814
xac <jiangfengxi.c@gmail.com>
2022-05-29 16:55:40 +0800

    add comments for logger

--- src/logger.h
+++ src/logger.h
@@ -56,7 +56,7 @@ private:
     uint32_t thread_id_; // thread id
     std::string thread_name_; // thread name
     uint32_t fiber_id_; // fiber id
-    std::stringstream content_;
+    std::stringstream content_; // content
 
 };
 
@@ -67,10 +67,11 @@ public:
     typedef std::shared_ptr<Formatter> SharedPtr;
     Formatter() { PatternParse(); }
     Formatter(const std::string& pattern);
+    // output the event as formatted
     void Format(
         std::ostream& os, std::shared_ptr<Logger> logger, 
         LogLevel::Level level, LogEvent::SharedPtr event);
-    class FormatItem {
+    class FormatItem { // every format item has `Format` method to output their content
     public:
         typedef std::shared_ptr<FormatItem> SharedPtr;
         //FormatItem(const std::string& str = "") {}
@@ -80,24 +81,30 @@ public:
             LogLevel::Level level, LogEvent::SharedPtr event) = 0;
     };
 private:
-    std::string pattern_ = std::string("[%p]%d{%Y-%m-%d %H:%M:%S}%T(tid)%t%T(tname)%N%T(fid)%F%T[%c]%T%f:%l%T%m%n");
-    std::vector<FormatItem::SharedPtr> format_items_;
-    void PatternParse();
+    std::string pattern_ = std::string("[%p]%d{%Y-%m-%d %H:%M:%S}%T(tid)%t%T(tname)%N%T(fid)%F%T[%c]%T%f:%l%T%m%n"); // the pattern of formatter
+    std::vector<FormatItem::SharedPtr> format_items_; // formatted items
+    void PatternParse(); // parse the pattern to format items
 
 };
 
 
 
 class LogAppender {
+friend class Logger;
 public:
     typedef std::shared_ptr<LogAppender> SharedPtr;
     virtual ~LogAppender() {}
-    virtual void Log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::SharedPtr evnet) = 0;
+    // set the formatter of the appender
     void SetFormatter(Formatter::SharedPtr formatter) { formatter_ = formatter; }
+    // get the formatter of the appender
     Formatter::SharedPtr GetFormatter() { return formatter_; }
+    // set the level of the appender
     void SetLevel(LogLevel::Level level) { level_ = level; }
+    // get the level of the appender
     LogLevel::Level GetLevel() { return level_; }
 protected:
+    // set the event level and log it 
+    virtual void Log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::SharedPtr evnet) = 0;
     LogLevel::Level level_ = LogLevel::Level::DEBUG;
     Formatter::SharedPtr formatter_;
 };
@@ -109,38 +116,51 @@ public:
     typedef std::shared_ptr<Logger> SharedPtr;
     Logger() {}
     Logger(const std::string& logger_name, LogLevel::Level level);
-    void Log(LogLevel::Level level, LogEvent::SharedPtr event);
+    // set the event level as DEBUG and log the event
     void Debug(LogEvent::SharedPtr event);
+    // set the event level as INFO and log the event
     void Info(LogEvent::SharedPtr event);
+    // set the event level as WARNING and log the event
     void Warning(LogEvent::SharedPtr event);
+    // set the event level as ERROR and log the event
     void Error(LogEvent::SharedPtr event);
+    // set the event level as FATAL and log the event
     void Fatal(LogEvent::SharedPtr event);
+    // add a log appender to the logger
     void AddAppender(LogAppender::SharedPtr appender);
+    // delete a log appender to the logger
     void DeleteAppender(LogAppender::SharedPtr appender);
-    void ClearAppender() { log_appenders_.clear(); }
+    // clear all log appenders
+    void ClearAppenders() { log_appenders_.clear(); }
+    // get the logger name
     const std::string& GetName() { return logger_name_; }
 private:
-    const std::string logger_name_ = "root"; // default name
-    Formatter::SharedPtr formatter_ = std::make_shared<Formatter>(); // default formatter
-    LogLevel::Level level_ = LogLevel::Level::DEBUG; // default level
+    // default name
+    const std::string logger_name_ = "root"; 
+    // set the event level and log it
+    void Log(LogLevel::Level level, LogEvent::SharedPtr event);
+    // default formatter
+    Formatter::SharedPtr formatter_ = std::make_shared<Formatter>(); 
+    // default level
+    LogLevel::Level level_ = LogLevel::Level::DEBUG;
+    // the list of logappenders
     std::list<LogAppender::SharedPtr> log_appenders_;
 
 };
 
 class StdoutLogAppender : public LogAppender {
-public:
+private:
     void Log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::SharedPtr event) override;
-
 };
 
 class FileLogAppender : public LogAppender {
 public:
     FileLogAppender(const std::string file_name);
     bool ReopenFile();
-    void Log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::SharedPtr evnet) override;
 private:
     const std::string file_name_;
     std::ofstream file_stream_;
+    void Log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::SharedPtr evnet) override;
 };
 
 }
